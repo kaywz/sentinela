561df4b0af39ec5a2bd48f8b34f627115a7d943b
kaywz <kaillanysantos0704@gmail.com>
2026-08-21 12:18:35 -0300

    Add dynamic port configuration for server
    Updated server.js to include dynamic port configuration and maintain existing functionality.

--- backend/server.js
+++ backend/server.js
@@ -512,12 +512,8 @@ res.json(db.consultas);
 // INICIAR SERVIDOR
 
 
-app.listen(3000,()=>{
-
-
-console.log(
-"🏥 Hospital Pro rodando em http://localhost:3000"
-);
-
-
+const PORT = process.env.PORT
+             || 3000;
+app.listen(PORT,() => {
+  console.log(`Porta ${PORT}`);
 });
